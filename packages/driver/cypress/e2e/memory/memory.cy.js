bfc714a327b4a175fca1b39218b35cd8bc8a8b96
Alex Schwartz <alexschwartz01@gmail.com>
2023-12-25 12:35:50 -0500

    remove driver

--- packages/driver/cypress/e2e/memory/memory.cy.js
+++
@@ -1,7 +0,0 @@
-describe('memory spec', { browser: { family: 'chromium' } }, () => {
-  for (let index = 0; index < 50; index++) {
-    it(`test ${index + 1} passes`, () => {
-      cy.visit('http://localhost:3500/memory')
-    })
-  }
-})
