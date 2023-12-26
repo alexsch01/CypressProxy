dcb47cf1da3ac68359012da3004f28c39b08755b
Alex Schwartz <alexschwartz01@gmail.com>
2023-12-26 15:40:08 -0500

    almost done...

--- packages/types/src/auth.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authStateName = void 0;
-exports.authStateName = ['AUTH_COULD_NOT_LAUNCH_BROWSER', 'AUTH_ERROR_DURING_LOGIN', 'AUTH_BROWSER_LAUNCHED'];
--- packages/types/src/git.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.gitStatusType = void 0;
-exports.gitStatusType = ['modified', 'created', 'unmodified', 'noGitInfo'];
