a5ee3e865baf6caad5be4ccf128633d011a00ef1
Alex Schwartz <alexschwartz01@gmail.com>
2023-12-26 16:02:40 -0500

    removed majority of unneeded stuff

--- packages/https-proxy/index.js
+++ packages/https-proxy/index.js
@@ -1,5 +1,3 @@
-require(process.argv[1]+'/../packages/ts/register')
-
 module.exports = require('./lib/proxy')
 
 module.exports.CA = require('./lib/ca')
--- packages/network/index.js
+++ packages/network/index.js
@@ -1,5 +1,1 @@
-if (process.env.CYPRESS_INTERNAL_ENV !== 'production') {
-  require(process.argv[1]+'/../packages/ts/register')
-}
-
 module.exports = require('./lib')
