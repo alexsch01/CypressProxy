5a72d0662a93b94624ab9ccfea01f53b8d62700a
Alex Schwartz <alexschwartz01@gmail.com>
2023-12-26 15:30:00 -0500

    delete more stuff

--- packages/proxy/lib/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var request_middleware_1 = require("./http/request-middleware");
-var response_middleware_1 = require("./http/response-middleware");
